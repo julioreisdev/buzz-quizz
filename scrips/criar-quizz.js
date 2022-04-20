b816d4dcda311e39df0cc7b5e5f563b6f810f8ac
julioreisdev <juliosteam0352@gmail.com>
2022-04-20 00:51:33 -0300

    Adicionando script e preparando layout para a função de criar um quizz

---
+++ scrips/criar-quizz.js
@@ -0,0 +1,7 @@
+let telaChamada = document.querySelector('.criacao-quizz-chamada');
+let telaCriacao = document.querySelector('.criacao-quizz')
+
+function criarQuizzEtapa1() {
+    telaChamada.classList.add('none');
+    telaCriacao.classList.remove('none')
+}
