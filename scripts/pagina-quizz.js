142809d94604573e61ab54ab0cda907c08245204
EricChandia <eric.chandia.ti@gmail.com>
2022-04-23 10:44:16 -0300

    pagina quizz atualizada

--- scripts/pagina-quizz.js
+++ scripts/pagina-quizz.js
@@ -7,42 +7,9 @@ function carregarQuizz(quizzUnico){
     document.querySelector(".pagina-quizz").innerHTML = "";
     
     adicionaBanner(quizzClicado);
-    
-
-    for(let i = 0; i<quizzClicado.questions.length;i++)
-    {
-    console.log(i);
-    document.querySelector(".pagina-quizz").innerHTML += `
-    <div class="pg-qz-container-pergunta">
-    </div>`;
-    }
-
     adicionaPerguntas(quizzClicado);
 
-
-
-    // document.querySelector(".pg-qz-respostas").innerHTML = `
-    //     <div class="pg-qz-container-respostas">
-    //         <div class="pg-qz-resposta" onclick="clicarResposta(this)">
-    //             <img src="${quizzClicado.questions[i].answers[0].image}" alt="">
-    //             <div class="pg-qz-resposta-titulo">${quizzClicado.questions[i].answers[0].text}</div>
-    //         </div>
-    //         <div class="pg-qz-resposta">
-    //             <img src="${quizzClicado.questions[i].answers[1].image}" alt="">
-    //             <div class="pg-qz-resposta-titulo">${quizzClicado.questions[i].answers[1].text}</div>
-    //         </div>
-    //     </div>
-    //     <div class="pg-qz-container-respostas">
-    //         <div class="pg-qz-resposta">
-    //             <img src="${quizzClicado.questions[i].answers[2].image}" alt="">
-    //             <div class="pg-qz-resposta-titulo">${quizzClicado.questions[i].answers[2].text}</div>
-    //         </div>
-    //         <div class="pg-qz-resposta">
-    //             <img src="${quizzClicado.questions[i].answers[3].image}" alt="">
-    //             <div class="pg-qz-resposta-titulo">${quizzClicado.questions[i].answers[3].text}</div>
-    //         </div>
-    //     </div>`;
-
+    
 
 }
 
@@ -60,12 +27,22 @@ function adicionaBanner(quizzClicado){
     </div>`;
 }
 
+
 function adicionaPerguntas(quizzClicado){
+    //adiciona containers das perguntas
+    for(let i = 0; i<quizzClicado.questions.length;i++)
+    {
+    document.querySelector(".pagina-quizz").innerHTML += `
+    <div class="pg-qz-container-pergunta">
+    </div>`;
+    }
+
+    //adiciona perguntas dentro dos containers
     containerPerguntas = document.querySelectorAll(".pg-qz-container-pergunta");
     for(let i=0; i<quizzClicado.questions.length;i++)
     {
         containerPerguntas[i].innerHTML = `
-        <div class="pg-qz-pergunta style="background-color: ${quizzClicado.questions[i].color}">${quizzClicado.questions[i].title}
+        <div class="pg-qz-pergunta" style="background-color: ${quizzClicado.questions[i].color}">${quizzClicado.questions[i].title}
         </div>
         <div class="pg-qz-respostas">
         <div class="pg-qz-container-respostas">
